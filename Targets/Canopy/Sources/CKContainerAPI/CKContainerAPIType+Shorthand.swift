3eed386dff4272e4dd9f0247a22080073021a103
Jaanus Kase <jaanus@gmail.com>
2023-04-10 14:53:42 +0300

    CKContainer API

--- Targets/Canopy/Sources/CKContainerAPI/CKContainerAPIType+Shorthand.swift
+++ Targets/Canopy/Sources/CKContainerAPI/CKContainerAPIType+Shorthand.swift
@@ -1,7 +1,19 @@
 import CloudKit
 
 public extension CKContainerAPIType {
-  /// Shorthand fetchShareParticipants.
+  /// Fetch `CKShare.Participant` based on user record name or other info.
+  ///
+  /// This is one step of implementing a custom record sharing interface. You obtain share participants and add them to a `CKShare`.
+  ///
+  /// For more info, see [CKFetchShareParticipantsOperation](https://developer.apple.com/documentation/cloudkit/ckfetchshareparticipantsoperation/). Canopy internally calls this operation to execute this function.
+  ///
+  /// - Parameters:
+  ///   - lookupInfos: An array of user lookup infos to resolve into share participants.
+  ///   - qualityOfService: The desired quality of service of the request. Defaults to `.default` if not provided.
+  ///
+  /// - Returns:
+  ///     An array of `CKShare.Participant` values for the requested look up infos, or `CKRecordError` if there
+  ///     was an error with the request.
   func fetchShareParticipants(
     with lookupInfos: [CKUserIdentity.LookupInfo],
     qualityOfService: QualityOfService = .default
@@ -12,7 +24,19 @@ public extension CKContainerAPIType {
     )
   }
   
-  /// Shorthand acceptShares.
+  /// Obtain access to a CKShare after the system has prompted the user to accept the share.
+  ///
+  /// After the user uses the OS-provided interface to accept joining a shared record in CloudKit, your app is called with the share metadata. You must then use this `acceptShares` call, to convert the metadata into a real share which you then have access to.
+  ///
+  /// For more info, see [CKAcceptSharesOperation.](https://developer.apple.com/documentation/cloudkit/ckacceptsharesoperation) Canopy internally calls this operation to execute this function.
+  ///
+  /// - Parameters:
+  ///   - with: An array of `CKShare.Metadata` that you received e.g from the underlying system, which to convert into shares.
+  ///   - qualityOfService: The desired quality of service of the request. Defaults to `.default` if not provided.
+  ///
+  /// - Returns:
+  ///   An array of `CKShare` that corresponds to the array of given metadatas, or `CKRecordError` if there was an error with the request.
+  ///
   func acceptShares(
     with metadatas: [CKShare.Metadata],
     qualityOfService: QualityOfService = .default
--- Targets/Canopy/Sources/CKContainerAPI/CKContainerAPIType.swift
+++ Targets/Canopy/Sources/CKContainerAPI/CKContainerAPIType.swift
@@ -6,9 +6,30 @@ public enum CKContainerAPIError: Error {
   case onlyOneAccountStatusStreamSupported
 }
 
-/// Canopy overlay to the CKContainer API.
+/// Canopy API provider for CKContainer.
+///
+/// [CKContainer](https://developer.apple.com/documentation/cloudkit/ckcontainer)
+/// is the main unit of abstaction for your app’s data in CloudKit. It has methods to get information about the
+/// current CloudKit user and other users.
+///
+/// Some methods of this protocol have a preferred shorthand way of calling them via a protocol extension,
+/// which lets you skip specifying some parameters and provides reasonable default values for them.
+///
+/// To access your app’s actual data in CloudKit, see ``CKDatabaseAPIType``.
 public protocol CKContainerAPIType {
   /// Obtain the user record ID for the current CloudKit user.
+  ///
+  /// You don’t need to do this for regular CloudKit use. Your app doesn’t need to know anything about the current user,
+  /// including their record ID, to use CloudKit.
+  ///
+  /// Knowing the current user record ID may be useful in scenarios related to record sharing and other communication
+  /// between your app’s CloudKit users.
+  ///
+  /// You can also store the record ID and compare it with future sessions of your app. The record ID may change if
+  /// the current user logs out in the device’s iCloud Settings, and another user logs in.
+  ///
+  /// The reported user ID is unique for the current user, your app, and CloudKit environment. CloudKit reports the same user record ID
+  /// for a given iCloud user across all of their devices.
   var userRecordID: Result<CKRecord.ID?, CKRecordError> { get async }
   
   /// Obtain CloudKit account status for the current user.
@@ -16,22 +37,20 @@ public protocol CKContainerAPIType {
   
   /// Obtain a stream of CloudKit account statuses of the current user.
   ///
-  /// To obtain this info with vanilla CloudKit API, you must listen to `CKAccountChanged` notifications,
-  /// and whenever you get one, use the `accountStatus` API of the CKContainer to find out what the
-  /// actual status is.
+  /// To obtain this info with vanilla CloudKit API, you must listen to [CKAccountChanged](https://developer.apple.com/documentation/foundation/nsnotification/name/1399172-ckaccountchanged)
+  /// notifications. Whenever you get one, you need to use the [accountStatus](https://developer.apple.com/documentation/cloudkit/ckcontainer/1399180-accountstatus)
+  /// API of the CKContainer to find out what the actual status is.
   ///
-  /// Canopy does all this internally, and provides you a stream of the statuses.
+  /// Canopy does all of this work internally, and provides you a simple stream of the account statuses.
   var accountStatusStream: Result<AsyncStream<CKAccountStatus>, CKContainerAPIError> { get async }
   
-  /// Fetch CKShare.Participant based on user record name or other info.
-  ///
-  /// After fetching a participant (or several participants), you can add them to CKShare.
-  /// This is the primary method how a shared record owner adds other members to the resource.
+  /// See ``CKContainerAPIType/fetchShareParticipants(with:qualityOfService:)`` for preferred way of calling this API.
   func fetchShareParticipants(
     with lookupInfos: [CKUserIdentity.LookupInfo],
     qos: QualityOfService
   ) async -> Result<[CKShare.Participant], CKRecordError>
-  
+
+  /// See ``CKContainerAPIType/acceptShares(with:qualityOfService:)`` for preferred way of calling this API.
   func acceptShares(
     with metadatas: [CKShare.Metadata],
     qos: QualityOfService
