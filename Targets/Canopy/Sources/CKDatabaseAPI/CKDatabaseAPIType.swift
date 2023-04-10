f877e733cbf7fd5ce93fd852e9fed8c374b7a004
Jaanus Kase <jaanus@gmail.com>
2023-04-10 16:40:28 +0300

    Documented CKDatabaseAPIType

--- Targets/Canopy/Sources/CKDatabaseAPI/CKDatabaseAPIType.swift
+++ Targets/Canopy/Sources/CKDatabaseAPI/CKDatabaseAPIType.swift
@@ -2,32 +2,42 @@ import CanopyTypes
 import CloudKit
 import Foundation
 
-/// Canopy overlay to the CKDatabase API.
+/// Canopy API provider for CKDatabase.
 ///
-/// FIXME: note how qos is written here, but qualityOfService in the shorthand. This is to disambiguate the APIs.
+/// [CKDatabase](https://developer.apple.com/documentation/cloudkit/ckdatabase)
+/// is the representation for containers of your app’s records and record zones in the cloud. You interact with CKDatabase
+/// to store and obtain `CKRecord` objects that represent your app’s data.
+///
+/// Methods of this protocol have a preferred shorthand way of calling them via a protocol extension,
+/// which lets you skip specifying some parameters and provides reasonable default values for them.
 public protocol CKDatabaseAPIType {
+  
   typealias PerRecordProgressBlock = (CKRecord, Double) -> Void
   typealias PerRecordIDProgressBlock = (CKRecord.ID, Double) -> Void
-  
+
+  /// See ``CKDatabaseAPIType/queryRecords(with:in:qualityOfService:)`` for preferred way of calling this API.
   func queryRecords(
     with query: CKQuery,
     in zoneID: CKRecordZone.ID?,
     qos: QualityOfService
   ) async -> Result<[CKRecord], CKRecordError>
-  
+
+  /// See ``CKDatabaseAPIType/modifyRecords(saving:deleting:perRecordProgressBlock:qualityOfService:)`` for preferred way of calling this API.
   func modifyRecords(
     saving recordsToSave: [CKRecord]?,
     deleting recordIDsToDelete: [CKRecord.ID]?,
     perRecordProgressBlock: PerRecordProgressBlock?,
     qos: QualityOfService
   ) async -> Result<ModifyRecordsResult, CKRecordError>
-  
+
+  /// See ``CKDatabaseAPIType/deleteRecords(with:in:qualityOfService:)`` for preferred way of calling this API.
   func deleteRecords(
     with query: CKQuery,
     in zoneID: CKRecordZone.ID?,
     qos: QualityOfService
   ) async -> Result<ModifyRecordsResult, CKRecordError>
   
+  /// See ``CKDatabaseAPIType/fetchRecords(with:desiredKeys:perRecordIDProgressBlock:qualityOfService:)`` for preferred way of calling this API.
   func fetchRecords(
     with recordIDs: [CKRecord.ID],
     desiredKeys: [CKRecord.FieldKey]?,
@@ -35,31 +45,37 @@ public protocol CKDatabaseAPIType {
     qos: QualityOfService
   ) async -> Result<FetchRecordsResult, CKRecordError>
 
+  /// See ``CKDatabaseAPIType/modifyZones(saving:deleting:qualityOfService:)`` for preferred way of calling this API.
   func modifyZones(
     saving recordZonesToSave: [CKRecordZone]?,
     deleting recordZoneIDsToDelete: [CKRecordZone.ID]?,
     qos: QualityOfService
   ) async -> Result<ModifyZonesResult, CKRecordZoneError>
 
+  /// See ``CKDatabaseAPIType/fetchZones(with:qualityOfService:)`` for preferred way of calling this API.
   func fetchZones(
     with recordZoneIDs: [CKRecordZone.ID],
     qos: QualityOfService
   ) async -> Result<[CKRecordZone], CKRecordZoneError>
   
+  /// See ``CKDatabaseAPIType/fetchAllZones(qualityOfService:)`` for preferred way of calling this API.
   func fetchAllZones(
     qos: QualityOfService
   ) async -> Result<[CKRecordZone], CKRecordZoneError>
   
+  /// See ``CKDatabaseAPIType/modifySubscriptions(saving:deleting:qualityOfService:)`` for preferred way of calling this API.
   func modifySubscriptions(
     saving subscriptionsToSave: [CKSubscription]?,
     deleting subscriptionIDsToDelete: [CKSubscription.ID]?,
     qos: QualityOfService
   ) async -> Result<ModifySubscriptionsResult, CKSubscriptionError>
   
+  /// See ``CKDatabaseAPIType/fetchDatabaseChanges(qualityOfService:)`` for preferred way of calling this API.
   func fetchDatabaseChanges(
     qos: QualityOfService
   ) async -> Result<FetchDatabaseChangesResult, CanopyError>
 
+  /// See ``CKDatabaseAPIType/fetchZoneChanges(recordZoneIDs:fetchMethod:qualityOfService:)`` for preferred way of calling this API.
   func fetchZoneChanges(
     recordZoneIDs: [CKRecordZone.ID],
     fetchMethod: FetchZoneChangesMethod,
--- Targets/Canopy/Sources/Results/ModifyRecordsResult.swift
+++ Targets/Canopy/Sources/Results/ModifyRecordsResult.swift
@@ -1,7 +1,12 @@
 import CloudKit
 
+/// Successful result of record modification and deletion functions, containing details about saved and deleted records.
 public struct ModifyRecordsResult: Equatable {
+  /// An array of saved records. The records likely have different metadata from the records that you gave to the modification function
+  /// as input, because CloudKit updates the record modification timestamp and change tag on the server side when saving records.
   public let savedRecords: [CKRecord]
+  
+  /// Array of deleted record ID-s. This matches the array record ID-s that you gave to the function as input.
   public let deletedRecordIDs: [CKRecord.ID]
   
   public init(savedRecords: [CKRecord], deletedRecordIDs: [CKRecord.ID]) {
