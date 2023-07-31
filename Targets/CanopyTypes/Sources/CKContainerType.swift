26e0a7970817a447809d7158ed03788e19ab1579
Jaanus Kase <jaanus@gmail.com>
2023-07-31 14:43:36 +0300

    Fixed parameter sendability warning
    CKContainer API-s at some point added Sendable annotation to the callback blocks, so adding this also to the matching Canopy protocol.

--- Targets/CanopyTypes/Sources/CKContainerType.swift
+++ Targets/CanopyTypes/Sources/CKContainerType.swift
@@ -1,7 +1,7 @@
 import CloudKit
 
 public protocol CKContainerType {
-  func fetchUserRecordID(completionHandler: @escaping (CKRecord.ID?, Error?) -> Void)
-  func accountStatus(completionHandler: @escaping (CKAccountStatus, Error?) -> Void)
+  func fetchUserRecordID(completionHandler: @escaping @Sendable (CKRecord.ID?, Error?) -> Void)
+  func accountStatus(completionHandler: @escaping @Sendable (CKAccountStatus, Error?) -> Void)
   func add(_ operation: CKOperation)
 }
