a8f3b28addc2da266799c78fc2f8fce88c14abfb
Jaanus Kase <jaanus@studio-sequoia-beta.local>
2024-07-25 14:53:38 +0300

    New type stubs

---
+++ Targets/CanopyTypes/Sources/Result/CanopyResultRecordType.swift
@@ -0,0 +1,18 @@
+import CloudKit
+
+protocol CanopyRecordValueGetting {
+  subscript(_ key: String) -> (any CKRecordValueProtocol)? { get }
+}
+
+protocol CanopyResultRecordType: CanopyRecordValueGetting {
+  var encryptedValuesView: CanopyRecordValueGetting { get }
+  var recordID: CKRecord.ID { get }
+  var recordType: CKRecord.RecordType { get }
+  var creationDate: Date? { get }
+  var creatorUserRecordID: CKRecord.ID? { get }
+  var modificationDate: Date? { get }
+  var lastModifiedUserRecordID: CKRecord.ID? { get }
+  var recordChangeTag: String? { get }
+  var parent: CKRecord.Reference? { get }
+  var share: CKRecord.Reference? { get }
+}
---
+++ Targets/CanopyTypes/Sources/Result/MockCanopyResultRecord.swift
@@ -0,0 +1,62 @@
+import CloudKit
+
+struct MockValueStore: CanopyRecordValueGetting, @unchecked Sendable {
+  let values: [String: CKRecordValueProtocol]
+  
+  init(values: [String : CKRecordValueProtocol]) {
+    self.values = values
+  }
+  
+  subscript(_ key: String) -> (any CKRecordValueProtocol)? {
+    values[key]
+  }
+}
+
+struct MockCanopyResultRecord: CanopyResultRecordType, Sendable {
+  let encryptedValuesStore: MockValueStore
+  let valuesStore: MockValueStore
+  
+  let recordID: CKRecord.ID
+  let recordType: CKRecord.RecordType
+  let creationDate: Date?
+  let creatorUserRecordID: CKRecord.ID?
+  let modificationDate: Date?
+  let lastModifiedUserRecordID: CKRecord.ID?
+  let recordChangeTag: String?
+  let parent: CKRecord.Reference?
+  let share: CKRecord.Reference?
+  
+  init(
+    recordID: CKRecord.ID = .init(recordName: "mockRecordName"),
+    recordType: CKRecord.RecordType,
+    creationDate: Date? = nil,
+    creatorUserRecordID: CKRecord.ID? = nil,
+    modificationDate: Date? = nil,
+    lastModifiedUserRecordID: CKRecord.ID? = nil,
+    recordChangeTag: String? = nil,
+    parent: CKRecord.Reference? = nil,
+    share: CKRecord.Reference? = nil,
+    values: [String: CKRecordValueProtocol] = [:],
+    encryptedValues: [String: CKRecordValueProtocol] = [:]
+  ) {
+    self.recordID = recordID
+    self.recordType = recordType
+    self.creationDate = creationDate
+    self.creatorUserRecordID = creatorUserRecordID
+    self.modificationDate = modificationDate
+    self.lastModifiedUserRecordID = lastModifiedUserRecordID
+    self.recordChangeTag = recordChangeTag
+    self.parent = parent
+    self.share = share
+    valuesStore = MockValueStore(values: values)
+    encryptedValuesStore = MockValueStore(values: encryptedValues)
+  }
+  
+  subscript(_ key: String) -> (any CKRecordValueProtocol)? {
+    valuesStore[key]
+  }
+  
+  var encryptedValuesView: CanopyRecordValueGetting {
+    encryptedValuesStore
+  }
+}
